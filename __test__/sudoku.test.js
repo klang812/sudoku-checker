7ed77a20749c0b3651ec44d22c850c9aa342db80
Kerry Lang <klang812@gmail.com>
2021-02-02 11:28:29 -0800

    add coverage file to gitignore
    tien96ng <tien96ng@gmail.com>

---
+++ __test__/sudoku.test.js
@@ -0,0 +1,9 @@
+import { TestScheduler } from "jest";
+import sudoku from "../src/sudoku.js";
+
+describe("sudoku", () => {
+  test("should return undefined", () => {
+    expect(sudoku([1,2,3,4,5,6,7,8,9])).toEqual([1,2,3,4,5,6,7,8,9]);
+  });
+
+});
