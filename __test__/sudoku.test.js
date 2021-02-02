8abb3b63628e26e04effa03a66fd012ea5547019
Kerry Lang <klang812@gmail.com>
2021-02-02 13:27:35 -0800

    test3 fail no repeat number sudoku
    tien96ng <tien96ng@gmail.com>

--- __test__/sudoku.test.js
+++ __test__/sudoku.test.js
@@ -1,14 +1,17 @@
-import { TestScheduler } from "jest";
 import sudoku from "../src/sudoku.js";
 
 describe("sudoku", () => {
   
   test("should return the array given", () => {
-    expect(sudoku([1,2,3,4,5,6,7,8,9])).toEqual([1,2,3,4,5,6,7,8,9]);
+    expect(sudoku([1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual(true);
   });
 
   test("should return the array given containing all numbers from 1 to 9.", () => {
-    expect(sudoku([1,2,3,4,5,6,7,8,9])).toEqual([1,2,3,4,5,6,7,9]);
+    expect(sudoku([1, 2, 3, 4, 5, 6, 9, 7, 8])).toEqual(true);
+  });
+
+  test("Should test given array contains no repeats", () => {
+    expect(sudoku([1, 2, 3, 4, 5, 6, 9, 8, 8])).toEqual(true);
   });
 
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,3 +1,12 @@
-export default function sudoku(num) {
-  return num;
+export default function sudoku(row) {
+  let sortedRow = row.sort();
+  if(sortedRow.length !== 9) {
+    return false;
+  }
+  for(let i = 0; i <= 8; i++) {
+        if(sortedRow[i] !== (i + 1)) {
+            return false;
+        }
+    }
+  return true;
 }
