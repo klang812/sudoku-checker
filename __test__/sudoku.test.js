2cca847cf3ba6afc1ba2b3d45199694eeadedab4
Kerry Lang <klang812@gmail.com>
2021-02-02 11:45:08 -0800

    test2 good fail sudoku
    tien96ng <tien96ng@gmail.com>

--- __test__/sudoku.test.js
+++ __test__/sudoku.test.js
@@ -2,8 +2,13 @@ import { TestScheduler } from "jest";
 import sudoku from "../src/sudoku.js";
 
 describe("sudoku", () => {
+  
   test("should return the array given", () => {
     expect(sudoku([1,2,3,4,5,6,7,8,9])).toEqual([1,2,3,4,5,6,7,8,9]);
+  });
+
+  test("should return the array given containing all numbers from 1 to 9.", () => {
+    expect(sudoku([1,2,3,4,5,6,7,8,9])).toEqual([1,2,3,4,5,6,7,9]);
   });
 
 });
